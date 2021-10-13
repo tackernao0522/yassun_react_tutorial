236b4c3efce315c6e2df79b7688573700f7ccf57
takaki <cheap_trick_magic@yahoo.co.jp>
2021-10-13 19:47:06 +0900

    親子館でのデータのやり取り/App.jsに言語データを移す/実際に追加--クラスでの記述

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { List } from "./List";
 import { Form } from "./Form";
+import { LANGUAGES } from './const/languages';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            tab: 'list', langs: LANGUAGES
+        };
+    }
+    addLang(lang) {
+        this.setState({
             tab: 'list',
-        }
+            langs: [...this.state.langs, lang]
+        });
     }
     render() {
-        const { tab } = this.state;
+        const { tab, langs } = this.state;
         return (
             <div>
                 <header>
                     <ul>
-                        <li onClick={ () => this.setState({ tab: 'list' }) }>リスト</li>
-                        <li onClick={ () => this.setState({ tab: 'form' }) }>フォーム</li>
+                        <li onClick={() => this.setState({ tab: 'list' })}>リスト</li>
+                        <li onClick={() => this.setState({ tab: 'form' })}>フォーム</li>
                     </ul>
                 </header>
                 <hr />
                 {
-                    tab === 'list' ? <List /> : <Form />
+                    tab === 'list' ? <List langs={langs} /> : <Form onAddLang={(lang) => this.addLang(lang)} />
                 }
             </div>
         )
--- src/List.js
+++ src/List.js
@@ -1,21 +1,13 @@
-import React from "react"
-
-const LANGUAGES = [
-    'JavaScript',
-    'C++',
-    'Ruby',
-    'Java',
-    'PHP',
-    'Go',
-];
+import React from "react";
 
 export class List extends React.Component {
     render() {
+        const { langs } = this.props;
         return (
             <div>
                 {
-                    LANGUAGES.map((lang, index) => {
-                        return <div key={index}>{ lang }</div>
+                    langs.map((lang, index) => {
+                        return <div key={index}>{lang}</div>
                     })
                 }
             </div>
